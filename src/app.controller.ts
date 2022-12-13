47554940d42bb11f9441cfc1c36e44be01b1ae88
Wenish <Wenish@users.noreply.github.com>
2022-12-14 00:58:39 +0100

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,4 +8,14 @@ export class AppController {
   getHello(): string {
     return '<h1>vland api</h1><p>running...</p>';
   }
+  
+  @Get('/swag')
+  getSwag(): string {
+    return '<iframe width="100%" height="100%" autoplay src="https://www.youtube.com/embed/drmXWo8ttrM?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  }
+
+  @Get('/gimu')
+  getGimu(): string {
+    return '<iframe width="100%" height="100%" autoplay src="https://www.youtube.com/embed/uepXPftPTaU?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  }
 }
