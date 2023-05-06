38e38732bd66d9e261b187debc34382a1ca5dc70
Wenish <Wenish@users.noreply.github.com>
2023-05-06 20:08:42 +0200

    Update buildings.controller.ts

--- src/buildings/buildings.controller.ts
+++ src/buildings/buildings.controller.ts
@@ -1,20 +1,28 @@
-import { Controller, Body, Delete } from '@nestjs/common';
+import { Controller, Body, Delete, BadRequestException } from '@nestjs/common';
 import { Put } from '@nestjs/common/decorators';
 import { ApiTags } from '@nestjs/swagger';
 import { BuildingsService } from './buildings.service';
 import { Building } from 'src/database/schemas/building.schema';
 import { BuildingPutDto } from './dto/building-put.dto';
 import { BuildingDeleteDto } from './dto/building-delete.dto';
+import { MongooseError } from 'mongoose';
 
 @ApiTags('buildings')
 @Controller('buildings')
 export class BuildingsController {
-  constructor(private readonly buildingsService: BuildingsService) {}
+  constructor(private readonly buildingsService: BuildingsService) { }
 
   @Put()
   async put(@Body() buildingPutDto: BuildingPutDto) {
-    const building = await this.buildingsService.put(buildingPutDto);
-    return building as Building;
+    try {
+      const building = await this.buildingsService.put(buildingPutDto);
+      return building as Building;
+    } catch (error) {
+      if (error instanceof MongooseError && error.name === 'ValidationError') {
+        throw new BadRequestException(error.message);
+      }
+      throw error;
+    }
   }
 
   @Delete()
