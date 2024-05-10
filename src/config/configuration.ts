f1489ba1b62fc97ec918bc6f2aaaa38ecfe6d5b1
Wenish <Wenish@users.noreply.github.com>
2024-05-11 01:17:30 +0200

    revert firebase key config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,19 +1,14 @@
-export default () => {
-  console.log(process.env.FIREBASE_PRIVATE_KEY)
-  const cleanKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/\\\\n/g, '\n')
-  console.log(cleanKey)
-    return {
-    port: parseInt(process.env.PORT, 10) || 3001,
-    database: {
-      mongodb: {
-        uri: process.env.MONGO_URL,
-        name: process.env.DATABASE_MONGODB_NAME || 'vland',
-      },
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3001,
+  database: {
+    mongodb: {
+      uri: process.env.MONGO_URL,
+      name: process.env.DATABASE_MONGODB_NAME || 'vland',
     },
-    firebase: {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    },
-  }
-};
+  },
+  firebase: {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  },
+});
