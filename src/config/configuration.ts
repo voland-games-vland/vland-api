8c742d78933243fb8d578336e2347b1e8e1f60d2
Wenish <Wenish@users.noreply.github.com>
2024-05-10 23:51:04 +0200

    update firebase key config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -9,6 +9,6 @@ export default () => ({
   firebase: {
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
   },
 });
